5d27d9999da50be82129fa3367835e717d9b21b4
Apurba <apurbadaria8@gmail.com>
2023-03-18 01:43:28 +0600

    updated

--- robinsonbd/settings.py
+++ robinsonbd/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+)abwzjlys+s(=5%z87s0&kib+_1c!hmy$&8ff9s9nq)vlt@q4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'robinsonbd.com',
